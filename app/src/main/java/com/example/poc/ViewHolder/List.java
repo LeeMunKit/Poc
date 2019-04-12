2de1b999b18b6257b980b6d0800d3c0f40a60b76
LeeMunKit <munkit.lee@softspace.com.my>
2019-04-12 15:37:14 +0800

    12/4 3:37

---
+++ app/src/main/java/com/example/poc/ViewHolder/List.java
@@ -0,0 +1,14 @@
+package com.example.poc.ViewHolder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.poc.R;
+
+public class List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
